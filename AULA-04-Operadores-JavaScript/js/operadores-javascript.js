b8ab0f424893446e0755a1eeee1fc04430df036f
Luciano Bitencourt <lucbite@gmail.com>
2022-02-10 17:02:21 -0300

    .

--- AULA-04-Operadores-JavaScript/js/operadores-javascript.js
+++ AULA-04-Operadores-JavaScript/js/operadores-javascript.js
@@ -32,3 +32,17 @@ console.log(`Resultado da operação de adicao dos valores ${valorNum1} % ${valo
 let  exponenciacao = (valorNum1**valorNum2);
 console.log(`Resultado da operação de adicao dos valores ${valorNum1} ** ${valorNum2} =   ${exponenciacao}`);
 
+// precedencia 
+
+let a = 5 + 3
+console.log(a);
+let b = a % 5
+console.log(b);
+let c = 5 * b**2
+console.log(c);
+let d = 10-a/2
+console.log(d);
+let e = 6 * 2 / d
+console.log(e);
+let f = b%e + 4/e
+console.log(f);
