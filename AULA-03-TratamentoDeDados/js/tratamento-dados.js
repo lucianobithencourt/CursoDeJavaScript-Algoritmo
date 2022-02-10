a00868e59c27ed51d2d7db0f69dcb113229c3beb
Luciano Bitencourt <lucbite@gmail.com>
2022-02-10 13:01:06 -0300

    .

--- AULA-03-TratamentoDeDados/js/tratamento-dados.js
+++ AULA-03-TratamentoDeDados/js/tratamento-dados.js
@@ -1,17 +1,26 @@
 
+//  .length conta quantidades 
+
+let $nome = prompt("Qual seu nome","Digite aqui")
+
+ alert(`Prazer te conhecer ${$nome}`)
+
+ alert(`Seu nome ${$nome} tem ${$nome.length} letras`)
+
+ alert(`Formas de deixar a string do codigo Maiusculo e minusculo usando toUppercase toLowercase `);
+
+ alert(`Ficando assim : ${$nome.toUpperCase()} com toUpperCase()`);
+ alert(`Ficando assim : ${$nome.toLowerCase()} com toLowerCase()`);
 
 
-// let $nome = prompt("Qual seu nome","Digite aqui")
+//  OPERAÇÃO SIMPLES DE SOMA 
+// let $num1 = Number(prompt("Digite um valor","Digite aqui"))
 
-// alert(`Prazer te conhecer ${$nome}`)
+// let $num2 = Number(prompt("Digite um valor","Digite aqui"))
 
-let $num1 = Number(prompt("Digite um valor","Digite aqui"))
+// let $soma = ($num1+$num2)
 
-let $num2 = Number(prompt("Digite um valor","Digite aqui"))
+// console.log(`A soma dos valores é igual ${$soma}`);
 
-let $soma = ($num1+$num2)
-
-console.log(`A soma dos valores é igual ${$soma}`);
-
-document.write(`A soma dos valores é igual ${$soma}`)
+// document.write(`A soma dos valores é igual ${$soma}`)
 
