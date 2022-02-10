f354211f9dd3a674239a9813e0264f7dd79bd40e
Luciano Bitencourt <lucbite@gmail.com>
2022-02-10 12:35:00 -0300

    .

--- AULA-03-TratamentoDeDados/js/tratamento-dados.js
+++ AULA-03-TratamentoDeDados/js/tratamento-dados.js
@@ -1,8 +1,17 @@
 
 
 
+// let $nome = prompt("Qual seu nome","Digite aqui")
 
+// alert(`Prazer te conhecer ${$nome}`)
 
+let $num1 = Number(prompt("Digite um valor","Digite aqui"))
 
+let $num2 = Number(prompt("Digite um valor","Digite aqui"))
 
+let $soma = ($num1+$num2)
+
+console.log(`A soma dos valores é igual ${$soma}`);
+
+document.write(`A soma dos valores é igual ${$soma}`)
 
