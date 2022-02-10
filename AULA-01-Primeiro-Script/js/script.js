49bed612057f4f80b74c60d752d186ba84bce40f
Luciano Bitencourt <lucbite@gmail.com>
2022-02-10 02:46:03 -0300

    .

---
+++ AULA-01-Primeiro-Script/js/script.js
@@ -0,0 +1,18 @@
+// Primeiro-Script
+// alert
+// window.document.write () podendo ser digitado assim tambem document.write()
+
+document.write('Minha idade é de ')
+window.document.write(45 + " anos <br>")
+
+document.write('a idade de Paula é ')
+document.write(32 + 'anos <br>')
+
+document.write('e a idade de Isadora é de 35 anos : <br>')
+
+idade = (45+32+35)
+media_idade = (45+32+35)/3
+
+document.write('a soma das idade é igual a ' + idade  + " anos de vida <br>")
+
+document.write('A média da idade dos adultos relacionados é de ' + media_idade.toFixed(2) + 'anos'  )
