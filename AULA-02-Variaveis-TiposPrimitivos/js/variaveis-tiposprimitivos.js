b9e8cc1fb0e4317d9be725b3b87d971e67e120d0
Luciano Bitencourt <lucbite@gmail.com>
2022-02-10 12:27:54 -0300

    .

--- AULA-02-Variaveis-TiposPrimitivos/js/variaveis-tiposprimitivos.js
+++ AULA-02-Variaveis-TiposPrimitivos/js/variaveis-tiposprimitivos.js
@@ -5,12 +5,17 @@
 
 /**
  * Tipos de variáveis em JavaScript
+ * 
 Os tipos de variáveis em JS são classificados em:
 Strings — Uma String nada mais é que texto puro.
 Numbers — São os números, seja eles integer, float, double etc.
+
 Booleans — São os operadores booleanos (true ou false)
+
 Arrays — É uma estrutura de dado para armazenar uma coleção de valores, sendo eles de qualquer tipo.
+
 Objects — Conjunto de atributos aninhados a uma variável denomina-se um objeto.
+
 Functions — Em JavaScript é possível declarar uma variável como uma função, podendo fazer operações e retornando o valor para a variável de declaração. Obs: muito utilizado no paradigma de programação funcional.
  */
 
@@ -27,11 +32,11 @@ Functions — Em JavaScript é possível declarar uma variável como uma funçã
 
 
 console.log("Tipos de variaveis JavaScript :");
-let element1 = "JavaScript"
-console.log(typeof element1);
-let element2 = 2022
-console.log(typeof element2);
-let element3 = true
-console.log(typeof element3);
+let $element1 = "JavaScript"
+console.log(typeof $element1);
+let $element2 = 2022
+console.log(typeof $element2);
+let $element3 = true
+console.log(typeof $element3);
 
 
