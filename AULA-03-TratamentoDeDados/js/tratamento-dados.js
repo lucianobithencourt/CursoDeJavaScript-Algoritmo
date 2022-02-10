20875a2deb019cae7282ca229ad56f36db173cb1
Luciano Bitencourt <lucbite@gmail.com>
2022-02-10 16:43:12 -0300

    .

--- AULA-03-TratamentoDeDados/js/tratamento-dados.js
+++ AULA-03-TratamentoDeDados/js/tratamento-dados.js
@@ -1,26 +1,43 @@
 
 //  .length conta quantidades 
+// .toUpperCase() DEIXA AS LETRAS MAIUSCULAS
+//  .toLowerCase() deixa as letras minusculas
 
-let $nome = prompt("Qual seu nome","Digite aqui")
+// let $nome = prompt("Qual seu nome","Digite aqui")
 
- alert(`Prazer te conhecer ${$nome}`)
+//  alert(`Prazer te conhecer ${$nome}`)
 
- alert(`Seu nome ${$nome} tem ${$nome.length} letras`)
+//  alert(`Seu nome ${$nome} tem ${$nome.length} letras`)
 
- alert(`Formas de deixar a string do codigo Maiusculo e minusculo usando toUppercase toLowercase `);
+//  alert(`Formas de deixar a string do codigo MAIUSCULO e minusculo usando toUppercase toLowercase `);
 
- alert(`Ficando assim : ${$nome.toUpperCase()} com toUpperCase()`);
- alert(`Ficando assim : ${$nome.toLowerCase()} com toLowerCase()`);
+//  alert(`Ficando assim : ${$nome.toUpperCase()} com toUpperCase()`);
+//  alert(`Ficando assim : ${$nome.toLowerCase()} com toLowerCase()`);
+
+
+//   Math.around() - arredonda valores decimais
+//  toFixed() - cola quantidade de numeros decimais pos ponto.
+// replace ('origim' , 'novoValor') troca valores 
+
+
 
 
 //  OPERAÇÃO SIMPLES DE SOMA 
-// let $num1 = Number(prompt("Digite um valor","Digite aqui"))
+ let $num1 = Number(prompt("Digite um valor","Digite aqui"))
 
-// let $num2 = Number(prompt("Digite um valor","Digite aqui"))
+let $num2 = Number(prompt("Digite um valor","Digite aqui"))
 
-// let $soma = ($num1+$num2)
+let $soma = ($num1+$num2)
 
-// console.log(`A soma dos valores é igual ${$soma}`);
+ console.log(`A soma dos valores é igual ${$soma}`);
 
-// document.write(`A soma dos valores é igual ${$soma}`)
+ console.log(`A soma dos valores é igual ${Math.round($soma)}`);
+
+ document.write(`A soma dos valores é igual ${$soma}<br>`)
+
+document.write(`A soma dos valores é igual ${Math.round($soma)}`);
+
+console.log(`A soma dos valores é igual ${$soma.toFixed(2)}`);
+ 
+ console.log(`A soma dos valores é igual ${Math.round($soma).toFixed(2)}`);
 
---
+++ AULA-04-Operadores-JavaScript/AULA-03-TratamentoDeDados/js/operadores-javascript.js
@@ -0,0 +1,34 @@
+// OPERADORES ARITMETICOS 
+
+/**
+ * +	Adição
+-	Subtração
+*	Multiplicação
+/	Divisão
+%	Módulo
+++	Incremento
+--	diminuição
+**  exponenciação
+ */
+
+let valorNum1 = 5;
+let valorNum2 = 2;
+
+let  adicao = (valorNum1+valorNum2);
+console.log(`Resultado da operação de adicao dos valores ${valorNum1} + ${valorNum2} =   ${adicao}`);
+
+let  subtracao = (valorNum1-valorNum2);
+console.log(`Resultado da operação de adicao dos valores ${valorNum1} - ${valorNum2} =   ${subtracao}`);
+
+let  multiplicacao = (valorNum1*valorNum2);
+console.log(`Resultado da operação de adicao dos valores ${valorNum1} * ${valorNum2} =   ${multiplicacao}`);
+
+let  divisao = (valorNum1/valorNum2);
+console.log(`Resultado da operação de adicao dos valores ${valorNum1} / ${valorNum2} =   ${divisao}`);
+
+let  modulo = (valorNum1%valorNum2);
+console.log(`Resultado da operação de adicao dos valores ${valorNum1} % ${valorNum2} =   ${modulo}`);
+
+let  exponenciacao = (valorNum1**valorNum2);
+console.log(`Resultado da operação de adicao dos valores ${valorNum1} ** ${valorNum2} =   ${exponenciacao}`);
+
