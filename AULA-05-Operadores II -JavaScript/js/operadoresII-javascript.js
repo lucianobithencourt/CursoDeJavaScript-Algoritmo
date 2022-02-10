c15ace61c3abe55af6317aa234753904110c33c9
Luciano Bitencourt <lucbite@gmail.com>
2022-02-10 18:45:22 -0300

    .

---
+++ AULA-05-Operadores II -JavaScript/js/operadoresII-javascript.js
@@ -0,0 +1,26 @@
+
+// operadore relacionais javascript
+
+let a = 5
+let b = '3'
+
+let maior = (a > b)
+console.log(maior);
+
+let menor = (a < b)
+console.log(menor);
+true
+let maiorigual = (a >= b)
+console.log(maiorigual);
+
+let menorigual = (a <= b)
+console.log(menorigual);
+true
+let igual = (a == b)
+console.log(igual);
+
+let diferente = (a != b)
+console.log(diferente);
+
+let identico = (a === b)
+console.log(identico);
