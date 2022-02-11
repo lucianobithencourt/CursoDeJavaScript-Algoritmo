782d5f53d131219216f01cede69dfd2eea4b283a
Luciano Bitencourt <lucbite@gmail.com>
2022-02-11 15:53:45 -0300

    .

---
+++ AULA-08-Eventos-DOM-Dados/js/dom-eventos.js
@@ -0,0 +1,26 @@
+
+// Manipuladores de eventos em JavaScript
+
+/**
+ * onclick
+Produz-se quando se clica o botão do mouse sobre um elemento da página, geralmente um botão ou um link.
+Javascript 1.0
+ */
+// -----------------------------------------------------
+function umclique() {
+    alert('evento ok');
+}
+
+// -----------------------------------------------------
+
+function clicarsoltar() {
+    alert('evento ok');
+}
+// -----------------------------------------------------
+function movermousa() {
+    alert('evento ok');
+}
+
+function selecaomouse() {
+    alert('selecionado....')
+}
