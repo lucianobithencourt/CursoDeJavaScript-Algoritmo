ef5558ed337821a7b335902d3df3f8b3a678cf71
Luciano Bitencourt <lucbite@gmail.com>
2022-02-12 18:51:45 -0300

    .

---
+++ AULA-10-Switch-Case/js/switch-case.js
@@ -0,0 +1,30 @@
+
+
+
+// switch-case
+
+let element = prompt (`Selecione uma das opções desejadas :  
+[01] - CALCULAR IMC -
+[02] - CALCULAR SUA IDADE -
+[03] - CONVERTER TEMPERATURAS -  
+`, `Digite aqui`)
+
+let opcao = Number(element)
+
+switch (opcao) {
+    case 1:
+        alert('opcao 1 - seleconada')
+        
+        break;
+
+        case 2:
+        alert('opcao 2 - seleconada')
+        break;
+
+        case 3:
+        alert('opcao 3 - seleconada')
+        break;
+
+    default:
+        break;
+}
