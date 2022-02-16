8c55c0bea996332bd057cd2d412f24d8327edbc5
Luciano Bitencourt <lucbite@gmail.com>
2022-02-16 17:51:50 -0300

    .

---
+++ AULA-11-Repeticao-While/js/while-do-while.js
@@ -0,0 +1,24 @@
+
+// // repetição while verificação no inicio
+
+// let n = 2
+// i = 0;
+// while (i <= 10) {
+//     console.log(`${n} X ${i} = `+n*i);
+//     i++
+// }
+
+
+// ---------------------------------------------
+
+// repeticao no final 
+let n = 2;
+let i = 0;
+
+do {
+        console.log(`${n} X ${i} = `+n*i);
+    
+    i++
+} while (i <=10);
+
+ 
