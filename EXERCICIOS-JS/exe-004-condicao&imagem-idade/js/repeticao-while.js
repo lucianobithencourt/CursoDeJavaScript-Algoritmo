bc4102dd6b8d99e2837f9c87f04081413541c5ce
Luciano Bitencourt <lucbite@gmail.com>
2022-02-16 11:48:18 -0300

    .

---
+++ EXERCICIOS-JS/exe-004-condicao&imagem-idade/js/repeticao-while.js
@@ -0,0 +1,22 @@
+
+
+
+
+// repetição while(enquanto)
+
+
+// let  i = 0;
+// while (i <=10) {
+//     console.log(i);
+//     i++
+// }
+
+// repetição no final 
+
+
+let i=0
+do {
+    console.log(i);
+} while (i <= 4);
+ i++
+
