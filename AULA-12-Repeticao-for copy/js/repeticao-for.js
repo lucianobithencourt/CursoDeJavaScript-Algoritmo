03c2913256155e2ba2f3fe4b5086b3aa0aacae2c
Luciano Bitencourt <lucbite@gmail.com>
2022-02-17 15:49:29 -0300

    .

---
+++ AULA-12-Repeticao-for copy/js/repeticao-for.js
@@ -0,0 +1,10 @@
+// repeticao for
+
+
+let numero = Number(prompt('tabuada de que numero que saber ? ', 'digite aqui'))
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(` ${numero} x ${i} = ${numero*i}`);
+    document.write(` ${numero} x ${i} = ${numero*i}<br> `)
+}
