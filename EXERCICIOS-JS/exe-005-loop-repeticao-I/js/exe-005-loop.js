51ba195cb6ecdb5d1d8e378111baf904747b478a
Luciano Bitencourt <lucbite@gmail.com>
2022-02-17 09:52:34 -0300

    .

---
+++ EXERCICIOS-JS/exe-005-loop-repeticao-I/js/exe-005-loop.js
@@ -0,0 +1,18 @@
+
+function contagem() {
+
+     let inicio = document.getElementById('inicio').value
+     let fim = document.getElementById('fim').value
+    //  let passo = document.getElementById('passo').value
+    
+    for ( i=inicio;  i < fim; i++) {
+        console.log(i);
+        
+        
+        
+    }
+        
+        
+   
+
+} // fim funccao
