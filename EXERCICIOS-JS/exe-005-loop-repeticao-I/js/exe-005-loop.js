205d76688ce2cf981ceaf153a36f0d9aedc2c83f
Luciano Bitencourt <lucbite@gmail.com>
2022-02-17 11:15:45 -0300

    .

--- EXERCICIOS-JS/exe-005-loop-repeticao-I/js/exe-005-loop.js
+++ EXERCICIOS-JS/exe-005-loop-repeticao-I/js/exe-005-loop.js
@@ -1,18 +1,26 @@
-
 function contagem() {
-
-     let inicio = document.getElementById('inicio').value
-     let fim = document.getElementById('fim').value
-    //  let passo = document.getElementById('passo').value
     
-    for ( i=inicio;  i < fim; i++) {
-        console.log(i);
-        
-        
-        
-    }
-        
-        
-   
+let inicio = Number(document.querySelector('#inicio').value)
+let fim = Number(document.querySelector('#fim').value)
+let passo = Number(document.querySelector('#passo').value)
 
-} // fim funccao
+if (inicio == '' || fim == '' || passo =='') {
+    alert('[ERRO] - FALTA DADOS')
+} else if (inicio == 0 || fim == 0 || passo == 0) {
+    alert('[ERRO] - PREENCHA COM VALORES OS CAMPOS')
+}
+
+for (i = inicio ; i <= fim ;  i+=passo  ) {
+    console.log(i);
+    let element = document.querySelector('#count-numbers')
+    element.innerHTML  +=` ${i} \u{1F449} ` // <-- colocar contagem
+    
+}
+
+
+
+
+
+
+
+}
