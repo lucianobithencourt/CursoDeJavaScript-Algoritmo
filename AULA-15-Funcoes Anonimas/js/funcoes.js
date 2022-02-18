97ca755297797d2701c87224b0c5e7e3e4d58bde
Luciano Bitencourt <lucbite@gmail.com>
2022-02-18 17:04:31 -0300

    .

---
+++ AULA-15-Funcoes Anonimas/js/funcoes.js
@@ -0,0 +1,52 @@
+// funcçoes
+
+/**
+ * Toda função tem uma chamada
+ * Toda funcao tem parametros de entrada
+ * Toada função tem uma ação ação 
+ * Toada função tem um retorno 
+ * 
+ */
+
+// function nome(nomepessoa) {
+    
+//     return nomepessoa
+// }
+
+// console.log(nome('felipe'));
+
+// -------------------------------------------------------
+
+/**
+ * function par_impar(vlr) {
+
+    let numero = Number(prompt("Digite um valor","Digite aqui"))
+
+    if (numero %2 == 0 ) {
+        console.log(`O numero ${numero} é um número par `);
+        alert(`O numero ${numero} é par `);
+    } else {
+         console.log(`O numero ${numero} é um número impar `);
+         alert(`O numero ${numero} é impar `);
+    }
+    
+    return  vlr
+}
+
+// chamada da função
+par_impar()
+
+ * 
+ */
+// -------------------------------------------------------
+
+/**
+ * let fat = 5
+
+for (let i = fat;  i >= 1  ; i--) {
+
+    console.log(i-1);
+    
+}
+
+ */
