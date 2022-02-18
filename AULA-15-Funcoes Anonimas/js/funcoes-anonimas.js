a73b735d7fbdc298342f8298428281ddbeba617a
Luciano Bitencourt <lucbite@gmail.com>
2022-02-18 19:12:29 -0300

    .

---
+++ AULA-15-Funcoes Anonimas/js/funcoes-anonimas.js
@@ -0,0 +1,18 @@
+// FUNÇÕES ANONIMAS
+
+// let msg  = function () {
+//     alert('ola')
+// }
+
+// msg()
+
+let calcularValores = function (vlr1,vlr2) {
+    
+    
+     let soma =  (vlr1+vlr2)
+     return soma
+
+
+}
+
+console.log(` a soma dos e igual á : ${calcularValores (2,9)}`);
